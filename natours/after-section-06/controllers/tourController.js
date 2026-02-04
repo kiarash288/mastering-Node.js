b583a51f46492f5c3fd4d15727c811d5ed416041
Kiarash <rabieekiarash7@gmail.com>
2026-02-04 00:58:28 -0800

    I added a first part of project (natours)

---
+++ natours/after-section-06/controllers/tourController.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, val) =>{
+  console.log(`Tour id is: ${val}`);
+  if(val * 1 > tours.length) {
+    res.status(404).json({
+      status: 'fall',
+      message: 'INVALID TOUR ID'
+    })
+  }
+  next();
+}
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    })
+  }
+  next();
+}
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+}
+exports.getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id );
+  res.status(200).json ({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err=> {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    }));
+  };
+
+
+  exports.updateTour = (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: '<Updated tour here...>'
+      }
+    });
+  };
+
+  exports.deleteTour = (req, res) => {
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  };) 
